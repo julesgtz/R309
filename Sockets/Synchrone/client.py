35665ba6e84fcf069a02735903fedbc0f3cfdcb7
jules <jules.goetz@uha.fr>
2023-11-07 09:32:56 +0100

    Deconnexion / Reconnexion OK synchrone

--- Sockets/Synchrone/client.py
+++ Sockets/Synchrone/client.py
@@ -1,18 +1,24 @@
 import socket
 
-client_socket = socket.socket()
 connected = False
 while True:
     if not connected:
+        client_socket = socket.socket()
         message = input("Tapes 'entrée' pour te connecter ")
-        client_socket.connect(("127.0.0.1", 6350))
+        client_socket.connect(("10.128.4.44", 6350))
     connected = True
     message = input("message : ")
     client_socket.send(message.encode())
     reply = client_socket.recv(1024).decode()
-    print(reply)
+    if reply:
+        print(f"Le serveur vous a envoyé le message suivant : {reply}")
 
     if message == "bye":
+        print("Fermeture de la connexion")
         client_socket.close()
         connected = False
+
+    if reply == "arret":
+        print("La connexion du serveur et du client s'est bien arrêtée")
+        client_socket.close()
 
--- Sockets/Synchrone/serveur.py
+++ Sockets/Synchrone/serveur.py
@@ -1,17 +1,27 @@
 import socket
 
 server_socket = socket.socket()
-server_socket.bind(("127.0.0.1", 6350))
+server_socket.bind(("10.128.4.44", 6350))
 server_socket.listen(1)
 conn, address = server_socket.accept()
-
+print("Succesfully connected")
 while True:
     message = conn.recv(1024).decode()
-    print(message)
-    reply = input("REPLY : ")
-    conn.send(reply.encode())
+    print(f"Vous avez recu un nouveau message : {message}")
 
     if message == "arret":
+        print("La connexion du serveur et du client s'est bien arrêtée")
         conn.close()
         server_socket.close()
         break
+
+    elif message == "bye":
+        conn.close()
+        print("Attente d'une nouvelle connexion")
+        server_socket.listen(1)
+        conn, address = server_socket.accept()
+        print("Succesfully connected")
+
+    else:
+        reply = input("Veuillez inserer une réponse : ")
+        conn.send(reply.encode())
