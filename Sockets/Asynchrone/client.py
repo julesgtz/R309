471019aa1053032d344a54e15c961257bdc675ad
jules <jules.goetz@uha.fr>
2023-11-07 10:01:16 +0100

    Client Asynchrone , Version a test

--- Sockets/Asynchrone/client.py
+++ Sockets/Asynchrone/client.py
@@ -1,23 +1,34 @@
 import socket
 from threading import Thread
 
+
 class Connexion:
     threads = []
 
     def __init__(self, ip: str, port: int):
+        self.ip = ip
+        self.port = port
+        self.stop = False
+        self.connect()
+
+    def connect(self):
         self.s = socket.socket()
-        self.s.connect((ip, port))
+        self.s.connect((self.ip, self.port))
+        print("Succesfully connected")
 
     def send_messages(self):
-        while True:
+        while not self.stop:
             message = input("Message à envoyer (ou 'arret' pour quitter) : ")
             self.s.send(message.encode())
-            if message == "arret":
+            if message == "bye":
                 print("Fermeture de la connexion")
                 self.s.close()
+                login = input("Voulez vous vous reconnecter ?")
+                if login:
+                    self.connect()
 
     def receive_messages(self):
-        while True:
+        while not self.stop:
             reply = self.s.recv(1024).decode()
             if reply:
                 print(f"Reçu du serveur: {reply}")
@@ -33,6 +44,7 @@ class Connexion:
         for t in self.threads:
             t.join()
 
+
 if __name__ == "__main__":
-    # Connexion(ip="10.128.4.44", port=6350).start()
     Connexion(ip="10.128.4.44", port=6350).start()
+    # Connexion(ip="10.128.4.42", port=6250).start()
